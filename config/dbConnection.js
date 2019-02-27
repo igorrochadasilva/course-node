074a1fd392bf42f96c4842ed50ba77fe38e90b4e
igor rocha <igot082011@hotmail.com>
2019-02-27 15:54:27 -0300

    updated node-course

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,10 +1,17 @@
 var mysql = require('mysql');
 
-module.exports = function(){
+//evitar execução da função createConnection sem que acontecer um autoload, foi necessario atribuir a uma variavel
+const comMySQL = function(){
+    console.log('conexão com banco de dados foi estabelecida')
     return connection = mysql.createConnection({
         host: 'localhost',
         user: 'root',
         password: '4321',
         database: 'portal_noticias'
     });
+ }
+
+module.exports = function(){
+    console.log('O autoload carregou o módulo de conexão com o BD');
+    return comMySQL;
 }
